3418b4eaa7a86ddc3c79ceadeb764a04c210d985
gustavoisaacc <gustavochaile7@gmail.com>
2020-07-12 18:21:42 -0300

    realizando la maqutacion about

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,35 @@
-console.log("hi, how's it going")
+import API from './api.js'
+const api = new API()
+
+const caracterContainer = document.querySelector('#caracter-container')
+
+class Caracter {
+    constructor({name, image}){
+        this.name = name
+        this.image = image
+        this.render()
+    }
+
+    build () {
+        return `
+        <article class="caracter">
+            <div class="caracter-grid">
+                <h2 class="caracter-name">${this.name}</h2>
+                <img src="${this.image}" alt="">
+            </div>
+        </article>
+        `
+    }
+
+    render(){
+        caracterContainer.innerHTML = this.build()
+    }
+}
+
+async function initApp(initCaracterId){
+    const caracterData = await api.getCaracter(initCaracterId)
+    // const rick = new Caracter(caracterData)
+
+}
+
+initApp(1)
